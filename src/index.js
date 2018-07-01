183b683862141903bae2dfdeefd88052526d5b45
ZWkang <907747874@qq.com>
2018-07-01 16:27:06 +0800

    ADDED: chow author name and change the path show

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ const message = require('./config.json');
 console.log(path.resolve('.'))
 
 console.log(path.resolve(`${message.age}`));
+console.log(path.resolve(__dirname,`${message.age} \n`),`Author: ${message.name}`);
